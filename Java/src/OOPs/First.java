c661878989101060fd128f2d6f2358d81e3f8daa
Lohi26 <lohitachandru@gmail.com>
2024-05-13 11:51:04 +0530

    Class and Object Creation

--- Java/src/OOPs/First.java
+++ Java/src/OOPs/First.java
@@ -1,4 +1,14 @@
 package OOPs;
 public class First {
-
+	public int a=10;
+	public int b=20;
+	public int add()
+	{
+		return a+b;
+	}
+	public static void main(String[] args)
+	{
+		First first=new First();
+		System.out.println(first.add());
+	}
 }
